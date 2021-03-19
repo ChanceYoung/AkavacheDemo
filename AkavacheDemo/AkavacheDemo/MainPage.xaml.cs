5b9c449c143b99b0b0a9ef9716eb66558ef4d150
Chance Young <ChanceTheHackerv2@gmail.com>
2021-03-19 12:31:11 -0600

    Add project files.

---
+++ AkavacheDemo/AkavacheDemo/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AkavacheDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSaveClicked(object sender, EventArgs args)
+        {
+
+        }
+        async void OnClearClicked(object sender, EventArgs args)
+        {
+
+        }
+    }
+}
